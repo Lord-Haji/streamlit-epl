1b997e178530cc8b8d0b7d93dfffec6ac93e92c2
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-17 16:19:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ st.set_page_config(
      initial_sidebar_state="expanded"
  )
  
- def home_page():
-    five38 = sd.FiveThirtyEight('ENG-Premier League', '2020')
-    games = five38.read_games() 
-    st.dataframe(games)
+def home_page():
+     five38 = sd.FiveThirtyEight('ENG-Premier League', '2020')
+     games = five38.read_games() 
+     st.dataframe(games)
  
- page_names_to_funcs = {
-    "Home Page": home_page   
+page_names_to_funcs = {
+     "Home Page": home_page   
 }
+
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+
 page_names_to_funcs[selected_page]()

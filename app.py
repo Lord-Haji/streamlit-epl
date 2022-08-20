404363b684f3e6941b1d8f3fe104c69502e4264b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-20 10:15:09 -0400

    adding pivot component

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import soccerdata as sd
 import streamlit_authenticator as stauth
+import streamlit.components.v1 as components
+from pivottablejs import pivot_ui
 
 st.set_page_config(
      page_title="streamlit-epl",
@@ -12,8 +14,10 @@ st.set_page_config(
 def epl():
      season = st.selectbox('Select EPL Season:',('2022','2021','2020'))
      five38 = sd.FiveThirtyEight('ENG-Premier League', season)
-     games = five38.read_games() 
-     st.dataframe(games)
+     games = five38.read_games()
+     games_pivot = pivot_ui(games)
+     with open(t.src) as t:
+         components.html(games_pivot.read(), width=900, height=1000, scrolling=True)
  
 page_names_to_funcs = {
      "EPL Analysis": epl   

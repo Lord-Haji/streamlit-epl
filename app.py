8c53fb42a34c413c07aa233cc937fd9ffae72771
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-20 10:17:55 -0400

    adding profiler

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import soccerdata as sd
 import streamlit_authenticator as stauth
-import streamlit.components.v1 as components
-from pivottablejs import pivot_ui
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(
      page_title="streamlit-epl",
@@ -15,9 +15,8 @@ def epl():
      season = st.selectbox('Select EPL Season:',('2022','2021','2020'))
      five38 = sd.FiveThirtyEight('ENG-Premier League', season)
      games = five38.read_games()
-     games_pivot = pivot_ui(games)
-     with open(t.src) as t:
-         components.html(games_pivot.read(), width=900, height=1000, scrolling=True)
+     games_pr = games.profile_report()
+     st_profile_report(games_pr)
  
 page_names_to_funcs = {
      "EPL Analysis": epl   
